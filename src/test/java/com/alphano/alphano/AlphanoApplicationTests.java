5d6b764965b8538bab6211509990e9a2f0fa4a32
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-08-05 20:35:12 +0900

    feat: 첫 커밋

---
+++ src/test/java/com/alphano/alphano/AlphanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphano.alphano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
