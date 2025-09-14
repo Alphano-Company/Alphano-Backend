4458c6655cf2880144262fb10b2e2360bc1ade3a
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-09-14 16:27:17 +0900

    feat: security 예외 추가

---
+++ src/main/java/com/alphano/alphano/security/exception/SecurityErrorCode.java
@@ -0,0 +1,29 @@
+package com.alphano.alphano.security.exception;
+
+import com.alphano.alphano.common.dto.ErrorReason;
+import com.alphano.alphano.common.exception.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.alphano.alphano.common.consts.AlphanoStatic.UNAUTHORIZED;
+
+@Getter
+@AllArgsConstructor
+public enum SecurityErrorCode implements BaseErrorCode {
+    INVALID_TOKEN(UNAUTHORIZED, "Invalid_Token", "유효하지 않은 토큰입니다."),
+    INVALID_SIGNATURE(UNAUTHORIZED, "Invalid_Signature", "유효하지 않은 서명입니다."),
+    REFRESH_EXPIRED(UNAUTHORIZED, "Refresh_Expired", "로그인이 만료되었습니다.");
+
+    private Integer status;
+    private String code;
+    private String reason;
+
+    @Override
+    public ErrorReason getErrorReason() {
+        return ErrorReason.builder()
+                .reason(reason)
+                .code(code)
+                .status(status)
+                .build();
+    }
+}
