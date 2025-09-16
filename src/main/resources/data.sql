bd48fe8cf9f5d605c5ea667754c2d144387e417f
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-09-16 14:59:51 +0900

    fix: @EnableJpaAuditing 활성화

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
+-- 문제 등록 (problem_id는 자동 생성)
 INSERT INTO problem(
-    problem_id,
     title,
     content,
     input_format,
@@ -9,24 +9,26 @@ INSERT INTO problem(
     created_at,
     updated_at
 ) VALUES
-      (1, 'ATAXX', '이 게임은 아주 엄청난 게임입니다.', '입력 형식', '출력 형식', 32, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-      (2, '두 번째 게임', '이 게임은 아주 엄청난 게임입니다.', '입력 형식', '출력 형식', 234, 12, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-      (3, '세 번째 게임', '이 게임은 아주 엄청난 게임입니다.', '입력 형식', '출력 형식', 45, 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+      ('ATAXX', '이 게임은 아주 엄청난 게임입니다.', '입력 형식', '출력 형식', 32, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+      ('두 번째 게임', '이 게임은 아주 엄청난 게임입니다.', '입력 형식', '출력 형식', 234, 12, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+      ('세 번째 게임', '이 게임은 아주 엄청난 게임입니다.', '입력 형식', '출력 형식', 45, 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
+-- 유저 등록 (user_id는 자동 생성)
 INSERT INTO users(
-    user_id,
     profile_image_url,
     identifier,
     password,
     nickname,
     created_at,
-    updated_at
+    updated_at,
+    role
 ) VALUES
-      (1, 'profile1.png', 'amily9011', '1234', 'cherry', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-      (2, 'profile2.png', 'amily9012', '1234', 'cherry2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+      ('profile1.png', 'amily9011', '1234', 'cherry', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'USER'),
+      ('profile2.png', 'amily9012', '1234', 'cherry2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'USER');
 
+-- 제출 등록 (submission_id는 자동 생성)
+-- FK(user_id, problem_id)는 위에서 INSERT한 순서대로 1, 2, 3... 값이 매겨짐
 INSERT INTO submission(
-    submission_id,
     user_id,
     problem_id,
     language,
@@ -38,5 +40,5 @@ INSERT INTO submission(
     created_at,
     updated_at
 ) VALUES
-      (1, 1, 1, 'c++', true, 0, 0, 0, 'key1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-      (2, 2, 1, 'c++', true, 0, 0, 0, 'key2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+      (1, 1, 'c++', true, 0, 0, 0, 'key1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+      (2, 1, 'c++', true, 0, 0, 0, 'key2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
