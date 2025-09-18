997f7ce1dd6e06db9859e69a31d4a9e051e206fd
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-09-18 20:52:03 +0900

    feat: 응답 dto 생성

--- src/main/java/com/alphano/alphano/domain/problem/dto/response/ProblemDetailResponse.java
+++ src/main/java/com/alphano/alphano/domain/problem/dto/response/ProblemDetailResponse.java
@@ -1,4 +1,11 @@
 package com.alphano.alphano.domain.problem.dto.response;
 
-public class ProblemDetailResponse {
-}
+public record ProblemDetailResponse (
+        Long problemId,
+        String title,
+        String content,
+        String inputFormat,
+        String outputFormat,
+        int submissionCount,
+        int submitterCount
+) {}
