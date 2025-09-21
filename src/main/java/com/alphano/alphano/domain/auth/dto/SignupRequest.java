e0bb7fa4cbbf37ff30b7e96495291ecd0da30490
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-09-22 00:30:07 +0900

    feat: 아이디 유효성 검사 추가

--- src/main/java/com/alphano/alphano/domain/auth/dto/SignupRequest.java
+++ src/main/java/com/alphano/alphano/domain/auth/dto/SignupRequest.java
@@ -1,9 +1,11 @@
 package com.alphano.alphano.domain.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record SignupRequest (
         @NotBlank
+        @Pattern(regexp = "^[a-zA-Z0-9]{3,20}$", message = "닉네임은 영문자와 숫자로 이루어진 3~20자여야 합니다.")
         String identifier,
 
         @NotBlank
