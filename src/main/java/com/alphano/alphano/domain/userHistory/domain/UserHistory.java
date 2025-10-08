87f8b442085406c808323a173ae416ce7d5e9430
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-10-08 22:37:35 +0900

    feat: UserHistory에 sigmoid 적용

--- src/main/java/com/alphano/alphano/domain/userHistory/domain/UserHistory.java
+++ src/main/java/com/alphano/alphano/domain/userHistory/domain/UserHistory.java
@@ -1,6 +1,7 @@
 package com.alphano.alphano.domain.userHistory.domain;
 
 import com.alphano.alphano.common.domain.BaseTimeEntity;
+import com.alphano.alphano.common.util.RatingUtils;
 import com.alphano.alphano.domain.match.domain.Match;
 import com.alphano.alphano.domain.problem.domain.Problem;
 import com.alphano.alphano.domain.user.domain.User;
@@ -19,6 +20,7 @@ public class UserHistory extends BaseTimeEntity {
     @Column(name = "user_history_id")
     private Long id;
 
+    // Applying the sigmoid function
     private Double ratingBefore;
     private Double ratingAfter;
     private Double ratingDelta;
@@ -40,13 +42,16 @@ public class UserHistory extends BaseTimeEntity {
     private Match match;
 
     public static UserHistory create(Match match, User user, Problem problem, double before, double after, Long opponentId, Outcome outcome) {
+        double alphanoBefore = RatingUtils.convert(before);
+        double alphanoAfter = RatingUtils.convert(after);
+
         UserHistory history =  UserHistory.builder()
                 .match(match)
                 .user(user)
                 .problem(problem)
-                .ratingBefore(before)
-                .ratingAfter(after)
-                .ratingDelta(after - before)
+                .ratingBefore(alphanoBefore)
+                .ratingAfter(alphanoAfter)
+                .ratingDelta(alphanoAfter - alphanoBefore)
                 .opponentId(opponentId)
                 .outcome(outcome)
                 .build();
